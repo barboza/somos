a9d213b37eec64aa2506f63fc6d7682aa6155407
Rafael Barboza <barboza.cardoso@gmail.com>
2013-03-01 14:02:58 -0300

    Creates User and Authorization models

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,9 +1,16 @@
 require 'machinist/active_record'
 
 # Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
+
+User.blueprint do
+  name {'Some nice guy'}
+  email {"someniceguy#{sn}@gmail.com"}
+  password {'niceguy'}
+  password_confirmation {'niceguy'}
+end
+
+Authorization.blueprint do
+  user {User.make!}
+  provider {"facebook"}
+  uid {10000}
+end
