d146b1494c74afdf42a7ae6ea1b2a9a9eea9f633
Rafael Barboza <barboza.cardoso@gmail.com>
2013-03-08 17:06:33 -0300

    Created the mission#index and misison partial

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,11 @@ class Mission < ActiveRecord::Base
   belongs_to :category
   attr_accessible :acomplished_at, :closed_at, :closing_message, :description, :goal, :image, :title, :category_id
   validates :title, :category, :goal, :description, presence: true
+
+  def short_description
+    first_line = description.split("\n").first
+    first_line = first_line[0..100] if first_line.length > 120
+    first_line += ' [...]' if first_line.length != description.length
+    first_line
+  end
 end
