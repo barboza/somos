68eee96a520c47161a0370c89871d6ac1484b69f
Rafael Barboza <barboza.cardoso@gmail.com>
2013-03-08 15:35:52 -0300

    added _form partial to missions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,6 @@
 class MissionsController < ApplicationController
   
   inherit_resources
-  actions :new, :create, :update
+  actions :all
+  before_filter :authenticate_user!, except: [:show, :index]
 end
