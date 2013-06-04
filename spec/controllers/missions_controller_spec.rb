a5799da0dd83e822a9da30134a7159482658289e
Jesus Mercado <jesusmmm@gmail.com>
2013-06-03 22:53:08 -0300

    Fixing controller test missions#show

--- spec/controllers/missions_controller_spec.rb
+++ spec/controllers/missions_controller_spec.rb
@@ -1,18 +1,43 @@
 require 'spec_helper'
 
 describe MissionsController do
+  include Devise::TestHelpers
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+
+  let(:mission) do
+    FactoryGirl.build(:mission)
+  end
 
   describe "GET 'show'" do
+    before do
+      mission.save!
+    end
+
     it "returns http success" do
-      get 'show'
+      get 'show', {id: mission.id}
       response.should be_success
+    end
+
+    it "renders template missions/show" do
+      get 'show', {id: mission.id}
+      response.should render_template('missions/show')
     end
   end
 
   describe "GET 'new'" do
-    pending "returns http success" do
+    it "returns http success" do
       get 'new'
       response.should be_success
+    end
+
+    it "renders template missions/new" do
+      get 'new'
+      response.should render_template('missions/new')
     end
   end
 
