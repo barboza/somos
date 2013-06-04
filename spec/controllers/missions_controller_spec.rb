600e8c23f8173b3528c75567ff08588cee0dbabe
Jesus Mercado <jesusmmm@gmail.com>
2013-06-03 22:51:22 -0300

    Mark as pending mission_controller tests

--- spec/controllers/missions_controller_spec.rb
+++ spec/controllers/missions_controller_spec.rb
@@ -10,35 +10,35 @@ describe MissionsController do
   end
 
   describe "GET 'new'" do
-    it "returns http success" do
+    pending "returns http success" do
       get 'new'
       response.should be_success
     end
   end
 
   describe "GET 'create'" do
-    it "returns http success" do
+    pending "returns http success" do
       get 'create'
       response.should be_success
     end
   end
 
   describe "GET 'edit'" do
-    it "returns http success" do
+    pending "returns http success" do
       get 'edit'
       response.should be_success
     end
   end
 
   describe "GET 'update'" do
-    it "returns http success" do
+    pending "returns http success" do
       get 'update'
       response.should be_success
     end
   end
 
   describe "GET 'delete'" do
-    it "returns http success" do
+    pending "returns http success" do
       get 'delete'
       response.should be_success
     end
